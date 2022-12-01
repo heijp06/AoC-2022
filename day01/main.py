4fb2f7207980b48bc4c2433fea8e37d086a68ec5
Peter van der Heijden <peter@vanderheijden.name>
2022-12-01 08:45:48 +0100

    Refactor main.py.

--- day01/main.py
+++ day01/main.py
@@ -4,8 +4,6 @@ from lib import part1, part2
 
 def read_rows(**kwargs):
     with open('data.txt', newline='') as csv_file:
-        # return list(csv.reader(csv_file, **kwargs))
-        # return csv_file.read().strip()
         return csv_file.read().splitlines()
 
 
@@ -15,7 +13,7 @@ def clip(x):
     pyperclip.copy(x)
 
 
-rows = [row for row in read_rows()]
+rows = list(read_rows())
 x = part1(rows)
 print(f"Part 1: {x}")
 clip(x)
