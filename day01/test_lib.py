94ec7c531ea6bf7e6f4d25049bf581197c239169
Peter van der Heijden <peter@vanderheijden.name>
2022-12-01 08:57:35 +0100

    Implement tests.

--- day01/test_lib.py
+++ day01/test_lib.py
@@ -1,10 +1,27 @@
-import pytest
 from lib import part1, part2
 
 
 def test_part1():
-    pass
+    assert part1(rows) == 24000
 
 
 def test_part2():
-    pass
+    assert part2(rows) == 45000
+
+
+rows = [
+    "1000",
+    "2000",
+    "3000",
+    "",
+    "4000",
+    "",
+    "5000",
+    "6000",
+    "",
+    "7000",
+    "8000",
+    "9000",
+    "",
+    "10000"
+]
