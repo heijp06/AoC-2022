a6c11d1e798fe5d01e1f8d236d203934fd174dfb
Peter van der Heijden <peter@vanderheijden.name>
2022-12-03 07:30:06 +0100

    Refactor tests.

--- day03/test_lib.py
+++ day03/test_lib.py
@@ -1,25 +1,37 @@
 import pytest
-from lib import part1, part2, get_badge
+from lib import part1, part2, get_badge, get_item
+
+
+data = [
+    ("vJrwpWtwJgWrhcsFMMfFFhFp", "p"),
+    ("jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL", "L"),
+    ("PmmdzqPrVvPwwTWBwg", "P"),
+    ("wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn", "v"),
+    ("ttgJtRGJQctTZtZT", "t"),
+    ("CrZsJsPPZsGzwwsLwLmpwMDw", "s")
+]
 
 
 def test_part1():
-    assert part1(rows) == 157
+    assert part1(get_rucksacks(data)) == 157
 
 
 def test_part2():
-    assert part2(rows) == 70
+    assert part2(get_rucksacks(data)) == 70
 
 
-def test_get_badge():
-    assert get_badge(rows[:3]) == "r"
-    assert get_badge(rows[3:]) == "Z"
+@pytest.mark.parametrize("rucksack,item", data)
+def test_get_item(rucksack: str, item: str) -> None:
+    assert get_item(rucksack) == item
 
 
-rows = [
-    "vJrwpWtwJgWrhcsFMMfFFhFp",
-    "jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL",
-    "PmmdzqPrVvPwwTWBwg",
-    "wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn",
-    "ttgJtRGJQctTZtZT",
-    "CrZsJsPPZsGzwwsLwLmpwMDw"
-]
+@pytest.mark.parametrize("tuples,badge", (
+    (data[:3], "r"),
+    (data[3:], "Z"),
+))
+def test_get_badge(tuples: list[tuple[str, str]], badge: str):
+    assert get_badge(get_rucksacks(tuples)) == badge
+
+
+def get_rucksacks(tuples: list[tuple[str, str]]):
+    return [tuple[0] for tuple in tuples]
