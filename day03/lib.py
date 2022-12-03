cfe5fb0d62074c91596fd9506b2b5cd50a440f95
Peter van der Heijden <peter@vanderheijden.name>
2022-12-03 07:22:07 +0100

    Cleanup code

--- day03/lib.py
+++ day03/lib.py
@@ -1,28 +1,27 @@
 def part1(rows: list[str]) -> int:
-    return sum(get_item(row) for row in rows)
+    return sum(get_priority(get_item(row)) for row in rows)
 
 
 def part2(rows: list[str]) -> int:
-    total = 0
-    for i in range(0, len(rows), 3):
-        total += get_priority(get_badge(rows[i:i + 3]))
-    return total
+    return sum(get_priority(get_badge(rows[i: i + 3])) for i in range(0, len(rows), 3))
 
 
-def get_item(row: str) -> int:
-    length = len(row) // 2
-    c1 = set(row[:length])
-    c2 = set(row[length:])
-    in_both = c1.intersection(c2)
-    return get_priority(next(iter(in_both)))
-
-
-def get_priority(item: str) -> int:
-    if item.isupper():
-        return ord(item) - ord('A') + 27
-    return ord(item) - ord('a') + 1
+def get_item(row: str) -> str:
+    compartment_size = len(row) // 2
+    compartment1 = set(row[:compartment_size])
+    compartment2 = set(row[compartment_size:])
+    in_both = compartment1.intersection(compartment2)
+    return get_single(in_both)
 
 
 def get_badge(rows: list[str]) -> str:
     in_all = set.intersection(*map(set, rows))
-    return next(iter(in_all))
+    return get_single(in_all)
+
+
+def get_single(intersection):
+    return next(iter(intersection))
+
+
+def get_priority(item: str) -> int:
+    return ord(item) + (1 - ord('a') if item.islower() else 27 - ord('A'))
--- day03/main.py
+++ day03/main.py
@@ -1,12 +1,9 @@
-import csv
 import pyperclip    # type: ignore
 from lib import part1, part2
 
 
 def read_rows(**kwargs):
     with open('data.txt', newline='') as csv_file:
-        # return list(csv.reader(csv_file, **kwargs))
-        # return csv_file.read().strip()
         return csv_file.read().splitlines()
 
 
@@ -16,7 +13,7 @@ def clip(x):
     pyperclip.copy(x)
 
 
-rows = [row for row in read_rows()]
+rows = list(read_rows())
 x = part1(rows)
 print(f"Part 1: {x}")
 clip(x)
