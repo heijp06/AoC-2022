c3a53a41f787d08853163e6fccbca5401e67811a
Peter van der Heijden <peter@vanderheijden.name>
2022-12-05 07:49:30 +0100

    Parse input.

---
+++ day05/lib.py
@@ -0,0 +1,40 @@
+import re
+from typing import NamedTuple
+
+Stack = list[str]
+
+
+class Step(NamedTuple):
+    repeat: int
+    start: int
+    end: int
+
+
+def part1(rows: list[str]) -> int:
+    stacks = parse_stacks(rows)
+
+
+def part2(rows: list[str]) -> int:
+    pass
+
+
+def parse_stacks(rows: list[str]) -> list[Stack]:
+    number_of_stacks = len(rows[0]) // 4 + 1
+    stacks = [[] for _ in range(number_of_stacks)]
+    for row in rows:
+        if row[1] == "1":
+            return stacks
+        for i in range(number_of_stacks):
+            index = 4 * i + 1
+            if row[index] != " ":
+                stacks[i].insert(0, row[index])
+
+
+def parse_steps(rows: list[str]) -> list[Step]:
+    steps = []
+    for row in rows:
+        if row and row[0] == "m":
+            repeat, start, end = re.match(
+                "move (\d*) from (\d) to (\d)", row).groups()
+            steps.append(Step(int(repeat), int(start), int(end)))
+    return steps
---
+++ day05/test_lib.py
@@ -0,0 +1,40 @@
+import pytest
+from lib import Step, parse_stacks, parse_steps, part1, part2
+
+
+def test_part1():
+    pass
+
+
+def test_part2():
+    pass
+
+
+def test_parse_stacks():
+    assert parse_stacks(rows) == [
+        ["Z", "N"],
+        ["M", "C", "D"],
+        ["P"],
+    ]
+
+
+def test_parse_steps():
+    assert parse_steps(rows) == [
+        Step(1, 2, 1),
+        Step(3, 1, 3),
+        Step(2, 2, 1),
+        Step(1, 1, 2)
+    ]
+
+
+rows = [
+    "    [D]    ",
+    "[N] [C]    ",
+    "[Z] [M] [P]",
+    " 1   2   3 ",
+    "",
+    "move 1 from 2 to 1",
+    "move 3 from 1 to 3",
+    "move 2 from 2 to 1",
+    "move 1 from 1 to 2"
+]
