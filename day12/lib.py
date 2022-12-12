e8004fe837f1aa7fc1cb3f26bec130f7f0a91df2
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-12 07:40:46 +0100

    Solve day 12 part 1.

--- day12/lib.py
+++ day12/lib.py
@@ -1,27 +1,45 @@
+from collections import namedtuple
+
+
 Grid = list[list[int]]
-Vector = tuple[int, int]
+Position = namedtuple("Position", "row,col")
+
 
 def part1(rows: list[str]) -> int:
     start, end, grid = parse(rows)
-    positions = [(0, 0)]
-    seen = {(0, 0)}
-    step = 0
+    height = len(grid)
+    width = len(grid[0])
+    if start == end:
+        return 0
+    positions = [start]
+    seen = {start}
+    step = 1
     while True:
-        for row, column in positions:
-            for drow, dcolumn in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                new_position = row + drow, column + dcolumn
-                if new_position in seen:
+        new_positions: list[Position] = []
+        for pos in positions:
+            for delta in [Position(0, 1), Position(0, -1), Position(1, 0), Position(-1, 0)]:
+                new_pos = Position(pos.row + delta.row, pos.col + delta.col)
+                if new_pos.row < 0 or new_pos.row >= height or new_pos.col < 0 or new_pos.col >= width:
                     continue
-
+                if new_pos in seen:
+                    continue
+                if grid[new_pos.row][new_pos.col] - grid[pos.row][pos.col] > 1:
+                    continue
+                if new_pos == end:
+                    return step
+                new_positions.append(new_pos)
+                seen.add(new_pos)
+        step += 1
+        positions = new_positions
 
 def part2(rows: list[str]) -> int:
     pass
 
 
-def parse(rows: list[str]) -> tuple[Vector, Vector, Grid]:
+def parse(rows: list[str]) -> tuple[Position, Position, Grid]:
     grid: Grid = []
-    start = -1
-    end = -1
+    start = Position(-1, -1)
+    end = Position(-1, -1)
     for row in range(len(rows)):
         line: list[int] = []
         grid.append(line)
@@ -29,10 +47,10 @@ def parse(rows: list[str]) -> tuple[Vector, Vector, Grid]:
             letter = rows[row][column]
             if letter == 'S':
                 line.append(0)
-                start = row, column
+                start = Position(row, column)
             elif letter == 'E':
                 line.append(25)
-                end = row, column
+                end = Position(row, column)
             else:
                 line.append(ord(letter) - ord('a'))
     return start, end, grid
