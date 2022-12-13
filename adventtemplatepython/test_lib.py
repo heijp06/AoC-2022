999ad7662c03945f3b677059e1368b58134c7195
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-13 16:50:34 +0100

    Add example.py and use it by default in test_lib.

--- adventtemplatepython/test_lib.py
+++ adventtemplatepython/test_lib.py
@@ -1,10 +1,11 @@
 import pytest
 from lib import part1, part2
+from example import RESULT1, RESULT2, example
 
 
 def test_part1():
-    pass
+    assert part1(example) == RESULT1
 
 
 def test_part2():
-    pass
+    assert part2(example) == RESULT2
