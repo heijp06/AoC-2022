4083fbee4d3ea99b1633236b8dae41814050899e
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-14 07:54:06 +0100

    Solve day 14 part 1.

---
+++ day14/cave.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+from collections import namedtuple
+import re
+
+Point = namedtuple("Point", "x,y")
+
+
+def parse(rows: list[str]) -> Cave:
+    cave: set[Point] = set()
+    y_max = 0
+    for row in rows:
+        coords = list(map(int, re.findall(r'\d+', row)))
+        point0 = Point(coords[0], coords[1])
+        for index in range(2, len(coords), 2):
+            point1 = Point(coords[index], coords[index + 1])
+            if point0.x == point1.x:
+                if point0.y < point1.y:
+                    start = point0
+                    end = point1
+                else:
+                    start = point1
+                    end = point0
+                for y in range(start.y, end.y + 1):
+                    cave.add(Point(point0.x, y))
+                y_max = max(y_max, end.y)
+            else:
+                if point0.x < point1.x:
+                    start = point0
+                    end = point1
+                else:
+                    start = point1
+                    end = point0
+                for x in range(start.x, end.x + 1):
+                    cave.add(Point(x, point0.y))
+                y_max = max(y_max, point0.y)
+            point0 = point1
+    return Cave(cave, y_max)
+
+
+class Cave:
+    def __init__(self, rocks: set[Point], y_max: int) -> None:
+        self.stuff = rocks
+        self.y_max = y_max
+        self.start = Point(500, 0)
--- day14/lib.py
+++ day14/lib.py
@@ -1,6 +1,29 @@
+from cave import Cave, Point, parse
+
+
 def part1(rows: list[str]) -> int:
-    pass
+    cave = parse(rows)
+    count = 0
+    while True:
+        sand = cave.start
+        while sand.y < cave.y_max:
+            new_sand = fall(sand, cave)
+            if new_sand == sand:
+                cave.stuff.add(sand)
+                count += 1
+                break
+            sand = new_sand
+        if sand.y >= cave.y_max:
+            return count
 
 
 def part2(rows: list[str]) -> int:
     pass
+
+
+def fall(sand: Point, cave: Cave) -> Point:
+    for delta in [0, -1, 1]:
+        new_sand = Point(sand.x + delta, sand.y + 1)
+        if new_sand not in cave.stuff:
+            return new_sand
+    return sand
