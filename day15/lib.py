e619a7e688d9add830ab7f59f1547f2f4e415b4d
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-15 13:31:54 +0100

    Solve day 15 part 2.

--- day15/lib.py
+++ day15/lib.py
@@ -1,9 +1,14 @@
 from sensor import Sensor, parse
 
+from collections import namedtuple
+
+Section = namedtuple("Section", "start, end")
+
 
 def part1(rows: list[str], row_to_check: int = 2000000) -> int:
     sensors = [parse(row) for row in rows]
     return no_beacons(row_to_check, sensors)
+
 
 def no_beacons(row: int, sensors: list[Sensor]):
     visible: set[int] = set()
@@ -16,4 +21,30 @@ def no_beacons(row: int, sensors: list[Sensor]):
 
 
 def part2(rows: list[str], size: int = 4000000) -> int:
-    pass
+    sensors = [parse(row) for row in rows]
+    for y in range(size + 1):
+        sections: list[Section] = []
+        for sensor in sensors:
+            length = sensor.width - abs(sensor.position.y - y)
+            if length < 0:
+                continue
+            start_x = max(0, sensor.position.x - length)
+            end_x = min(size, sensor.position.x + length)
+            section = Section(start_x, end_x)
+            new_sections = []
+            for other in sections:
+                if (section.start <= other.start <= section.end or section.start <= other.end <= section.end or 
+                    other.start <= section.start <= other.end or other.start <= section.end <= other.end):
+                    section = Section(
+                        min(section.start, other.start), max(section.end, other.end))
+                else:
+                    new_sections.append(other)
+            new_sections.append(section)
+            sections = new_sections
+        if sections != [Section(0, size)]:
+            if len(sections) == 1:
+                x = 0 if sections[0].start != 0 else size
+            else:
+                sections.sort()
+                x = sections[0].end + 1
+            return 4000000 * x + y
