5bbd02e0f86fb784fbe33073e38a871180bea575
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-18 14:17:11 +0100

    Make pylint and mypy a little happier.

--- day18/lib.py
+++ day18/lib.py
@@ -1,6 +1,8 @@
+from __future__ import annotations
 from collections import defaultdict
+from collections.abc import Iterable
 import re
-from typing import NamedTuple
+from typing import Iterator, NamedTuple
 
 
 class Point(NamedTuple):
@@ -9,15 +11,27 @@ class Point(NamedTuple):
     z: int
 
 
-Side = frozenset[Point]
+class Side(Iterable):
+    def __init__(self, point1: Point, point2: Point, point3: Point, point4: Point) -> None:
+        self.points = tuple(sorted([point1, point2, point3, point4]))
+
+    def __iter__(self) -> Iterator[Point]:
+        return iter(self.points)
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, Side) and self.points == other.points
+
+    def __hash__(self) -> int:
+        return hash(self.points)
+
 
 cube = (
-    frozenset((Point(0, 0, 0), Point(1, 0, 0), Point(1, 1, 0), Point(0, 1, 0))),
-    frozenset((Point(0, 0, 0), Point(1, 0, 0), Point(1, 0, 1), Point(0, 0, 1))),
-    frozenset((Point(0, 0, 0), Point(0, 1, 0), Point(0, 1, 1), Point(0, 0, 1))),
-    frozenset((Point(0, 0, 1), Point(0, 1, 1), Point(1, 1, 1), Point(1, 0, 1))),
-    frozenset((Point(0, 1, 1), Point(0, 1, 0), Point(1, 1, 0), Point(1, 1, 1))),
-    frozenset((Point(1, 1, 1), Point(1, 1, 0), Point(1, 0, 0), Point(1, 0, 1)))
+    Side(Point(0, 0, 0), Point(1, 0, 0), Point(1, 1, 0), Point(0, 1, 0)),
+    Side(Point(0, 0, 0), Point(1, 0, 0), Point(1, 0, 1), Point(0, 0, 1)),
+    Side(Point(0, 0, 0), Point(0, 1, 0), Point(0, 1, 1), Point(0, 0, 1)),
+    Side(Point(0, 0, 1), Point(0, 1, 1), Point(1, 1, 1), Point(1, 0, 1)),
+    Side(Point(0, 1, 1), Point(0, 1, 0), Point(1, 1, 0), Point(1, 1, 1)),
+    Side(Point(1, 1, 1), Point(1, 1, 0), Point(1, 0, 0), Point(1, 0, 1))
 )
 
 
@@ -26,12 +40,12 @@ def part1(rows: list[str]) -> int:
     for row in rows:
         offset = parse(row)
         for side in cube:
-            new_side = frozenset()
+            points: list[Point] = []
             for point in side:
                 new_point = Point(point.x + offset.x, point.y +
                                   offset.y, point.z + offset.z)
-                new_side = new_side | {new_point}
-            sides[new_side] += 1
+                points.append(new_point)
+            sides[Side(*points)] += 1
     return sum(seen for seen in sides.values() if seen == 1)
 
 
