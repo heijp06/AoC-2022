c1a952bcb188b8d5ed939d9538a217b7b68056f1
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-20 09:10:11 +0100

    Solve day 20 part 1.

--- day20/lib.py
+++ day20/lib.py
@@ -1,5 +1,16 @@
 def part1(rows: list[str]) -> int:
-    pass
+    values = [(index, int(row)) for index, row in enumerate(rows)]
+    new_values = list(values)
+    for key, value in values:
+        index = new_values.index((key, value))
+        new_values.remove((key, value))
+        new_index = (index + value) % (len(values) - 1)
+        if new_index == 0:
+            new_index = 0 if value >= 0 else (len(values) - 1)
+        new_values = new_values[:new_index] + [(key, value)] + new_values[new_index:]
+    result = [value for _, value in new_values]
+    index = result.index(0)
+    return sum(result[(index + offset) % len(result)] for offset in (1000, 2000, 3000))
 
 
 def part2(rows: list[str]) -> int:
