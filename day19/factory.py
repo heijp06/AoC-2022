37f58fcb980630c767970e271b6f44e7799e68c0
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-20 07:48:16 +0100

    Continue day 19.

--- day19/factory.py
+++ day19/factory.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 import re
-from typing import NamedTuple
+from typing import NamedTuple, Optional
 
 
 def parse(row: str) -> Factory:
@@ -19,30 +19,60 @@ class Blueprint(NamedTuple):
 
 
 class State(NamedTuple):
+    time: int
     ore: int
     clay: int
     obsidian: int
-    geode: int
+    geodes: int
     ore_robots: int
     clay_robots: int
     obsidian_robots: int
     geode_robots: int
 
+    def update_state(self,
+                     time: Optional[int] = None,
+                     ore: Optional[int] = None,
+                     clay: Optional[int] = None,
+                     obsidian: Optional[int] = None,
+                     geodes: Optional[int] = None,
+                     ore_robots: Optional[int] = None,
+                     clay_robots: Optional[int] = None,
+                     obsidian_robots: Optional[int] = None,
+                     geode_robots: Optional[int] = None
+                     ) -> State:
+        return State(
+            self.time or time,
+            self.ore or ore,
+            self.clay or clay,
+            self.obsidian or obsidian,
+            self.geodes or geodes,
+            self.ore_robots or ore_robots,
+            self.clay_robots or clay_robots,
+            self.obsidian_robots or obsidian_robots,
+            self.geode_robots or geode_robots,
+        )
+
 
 class Factory:
     def __init__(self, blueprint: Blueprint) -> None:
         self.blueprint = blueprint
-        self.start_state = State(0, 0, 0, 0, 1, 0, 0, 0)
+        self.start_state = State(1, 0, 0, 0, 0, 1, 0, 0, 0)
         self.max_geodes = -1
 
     def build(self) -> None:
         self.states = [self.start_state]
         self.seen = {self.start_state}
-        for _ in range(24):
-            self.new_states: list[State] = []
-            for state in self.states:
-                self.next_states(state)
-            self.states = self.new_states
+        self.new_states: list[State] = []
+        for state in self.states:
+            self.next_states(state)
+        self.states = self.new_states
 
     def next_states(self, state: State) -> None:
-        pass
+        # increase time
+        new_state = state.update_state(state.time + 1)
+
+        # if you want to build any robot first spend ore + rest.
+
+        # collect what the existing robots are producing
+
+        # if time > 24 stop
