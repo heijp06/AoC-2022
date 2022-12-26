e73655d0ac4708b6c25e3c271d0d63a37b409bf5
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-26 08:02:45 +0100

    Implement adding SNAFU numbers as a single operation instead of converting to and from decimal.

--- day25/lib.py
+++ day25/lib.py
@@ -1,26 +1,30 @@
+import functools
+import itertools
+
+
 digits = ["=", "-", "0", "1", "2"]
 
 
 def part1(rows: list[str]) -> str:
-    return to_snafu(sum(from_snafu(row) for row in rows))
+    return functools.reduce(add, rows, "0")
 
 
-def to_snafu(number: int) -> str:
-    value = number
-    snafu = ""
-    while value > 0:
-        value, digit = divmod(value, 5)
-        if digit > 2:
+def add(snafu1: str, snafu2: str) -> str:
+    result = ""
+    carry = 0
+    for digit1, digit2 in itertools.zip_longest(snafu1[::-1], snafu2[::-1], fillvalue="0"):
+        digit = digits.index(digit1) + digits.index(digit2) + carry - 4
+        if digit < -2:
+            digit += 5
+            carry = -1
+        elif digit > 2:
             digit -= 5
-            value += 1
-        snafu = digits[digit + 2] + snafu
-    return snafu
-
-
-def from_snafu(snafu: str) -> int:
-    value = 0
-    power = 1
-    for digit in snafu[::-1]:
-        value += (digits.index(digit) - 2) * power
-        power *= 5
-    return value
+            carry = 1
+        else:
+            carry = 0
+        result = digits[digit + 2] + result
+    if carry == -1:
+        result = f"-{result}"
+    elif carry == 1:
+        result = f"1{result}"
+    return result
--- day25/test_lib.py
+++ day25/test_lib.py
@@ -1,6 +1,15 @@
-from lib import part1
+import pytest
+from lib import add, part1
 from example import RESULT1, example
 
 
-def test_part1():
+def test_part1() -> None:
     assert part1(example) == RESULT1
+
+
+@pytest.mark.parametrize("snafu1,snafu2,expected", [
+    ("2", "1", "1="),  # 2 + 1 = 3
+    ("12", "2", "2-")  # 7 + 2 = 9
+])
+def test_add(snafu1: str, snafu2: str, expected: str) -> None:
+    assert add(snafu1, snafu2) == expected
