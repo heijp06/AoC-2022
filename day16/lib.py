358322e983d38c23d35f07725e23c0bdc1f396a1
Peter van der Heijden <peter.vd.heijden@panalytical.com>
2022-12-26 13:14:14 +0100

    Add timing.

--- day16/lib.py
+++ day16/lib.py
@@ -1,3 +1,4 @@
+import datetime
 from solver import Solver
 from valve import Valve
 
@@ -5,7 +6,10 @@ MINUTES = 30
 
 
 def part1(rows: list[str]) -> int:
+    t0 = datetime.datetime.now()
     solver = Solver(rows, 30)
+    t1 = datetime.datetime.now()
+    print(t1 - t0)
     return solver.solve(get_valves(solver))
 
 
